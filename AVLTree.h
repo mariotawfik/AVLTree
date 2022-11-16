755a876c7fac833da4fb70a2e4d9faa597958d0b
mariotawfik <amxmaroz@gmail.com>
2022-11-16 11:47:00 +0200

    updated comments

--- AVLTree.h
+++ AVLTree.h
@@ -13,15 +13,15 @@ private:
     AVLTree *left, *right, *parent; //Pointers to left and right children AVLTree Nodes and a pointer to the parent node;
     AVLTree* LeftRotate(); //Rotation of the nodes to the left
     AVLTree* RightRotate(); //Rotation of the nodes to the right
-    int ComputeUnbalanceLevel(); //The balance leve of the node, it must be between -1 and 1
+    int ComputeUnbalanceLevel(); //The balance level of the node, it must be between -1 and 1
 
 public:
     AVLTree(); //Sets value to null character which is char(0)
     ~AVLTree(); //deletes the children nodes and the node itself
-    AVLTree* Insert(int v); //Creates a new pointer then connects it depending on the value of char. Returns a pointer to the head of the tree.
+    AVLTree* Insert(int v); //Creates a new pointer then organizes the tree. Returns a pointer to the head of the tree.
     AVLTree *Find(int v); //Searches for v and returns the address
-    AVLTree *Delete(int v); //Searches for v, returns the address then removes it from the tree structure
-    void ToString(); //Displays the tree in a string structure
+    AVLTree *Delete(int v); //Searches for v, returns the address then removes it from the tree structure and organizes it
+    void ToString(); //Displays the node in a string structure
     bool Verify(); //Verifies that the tree is balanced by checking the balance of the head of the tree
 };
 
